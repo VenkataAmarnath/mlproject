84d47b825bd4159d2740bb21fab05919af4dcc29
VenkataAmarnath <pendelaamarnath@gmail.com>
2024-11-18 16:05:14 +0530

    logging and exception updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## End to End ML Project
+## End to End Machine Learning Project
+
+1. Create venv
+2. Create requirements.txt, setup.py, src folder with __init__.py file in it
+3. List down libraries required in requirements.txt
+4. Update setup.py
+5. create components in src/components folder: data_ingestion.py
+6. create files in src/pipeline folder: train_pipeline.py
+7. create utils.py, logger.py,exception.py files in src folder
+8. update exception.py
+9. update logger.py
+
---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys  # provides func and variables that are used to manipulate diff parts of python runtime environment
+import os
+from src.logger import logging
+
+def error_message_details(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()  # exc_tb give info:filename, line number of error
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    line_number=exc_tb.tb_lineno
+    
+
+    error_message=f"Error in Python script: filename-{file_name} line number-{line_number} error message-{str(error)}"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message) # since inheriting from Exception class
+        self.error_message=error_message_details(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+"""
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by zero")
+        raise CustomException(e,sys)
+
+"""
